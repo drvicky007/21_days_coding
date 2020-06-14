15f66cc5a7a16e2c94a04bb947022041bb81ae58
drvick007 <drvictoriaakilapa@gmail.com>
2020-06-13 21:09:27 -0700

    my bio data

---
+++ biodata.py
@@ -0,0 +1,11 @@
+def bio_data():
+    name = "Victoria Akilapa"
+    gender = "Female"
+    location = "GRA, Bauchi"
+    state = "Osun"
+    nationality = "Nigerian"
+    skills = "HTML, CSS, Python"
+    email = "drvictoriaakilapa@gmail.com"
+    print("my name is " +name+ ", i am a " +gender+ ",  i stay in " +location+ ", i am from " +state+ ", i am a " +nationality+ ", i have knowledge in " +skills+ ", you can reach me on " +email )
+
+bio_data()
--- python.py
+++ python.py
@@ -1,5 +1,14 @@
 print("HELLO WORLD")
 print("sage")
 
-if 5>2:
-    print ("Five is greater two!")
+if 5 > 2:
+    print("Five is greater two!")
+
+    #This is a comment.
+    print("Hello, Vicky!")
+
+    x = 5
+    y = "John"
+    print(x)
+    print(y)
+    
